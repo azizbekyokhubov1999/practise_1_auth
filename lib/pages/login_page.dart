b8e681627bcfee2b9f5795786dd457bba0b25fbe
Azizbek <azizbek.yoqubov.99@gmail.com>
2024-07-31 19:13:02 +0300

    login UI is added

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:practise_1_auth/components/my_button.dart';
+import 'package:practise_1_auth/components/square_tile.dart';
+
+import '../components/my_textfield.dart';
+
+class LoginPage extends StatelessWidget {
+   LoginPage({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  //sign in method
+   void signUserIn(){}
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.grey[300],
+      body:  SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 40),
+              // logo
+               const Icon(
+                 Icons.lock,
+                 size: 100,
+               ),
+              const SizedBox(height: 40),
+              //welcome back massage
+              Text("Welcome back you've been missed!",
+              style: TextStyle(
+                color: Colors.grey[700],
+                fontSize: 16,
+              ),
+              ),
+              const SizedBox(height: 20),
+              // username text field
+              MyTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+              const SizedBox(height: 10),
+              // password text field
+              MyTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+              const SizedBox(height: 10),
+              // forgot password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Colors.grey[600]
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              //sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+              const SizedBox(height: 40),
+              // or continue with other platform
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                 const Expanded(
+                  child: Divider(
+                    thickness: 1,
+                  ),
+                ),
+                 Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Text(
+                    "Or continue with",
+                    style: TextStyle(color: Colors.grey[700]),
+                  ),
+                ),
+                 const Expanded(
+                  child: Divider(
+                    thickness: 1,
+                  ),
+                ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40),
+
+              // google + apple sign in
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SquareTile(imagePath: 'images/img.png'),
+                  SizedBox(width: 10),
+                  SquareTile(imagePath: 'images/img_1.png')
+                ],
+              ),
+              const SizedBox(height: 35),
+              // not a member  register now
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Not a member?",
+                    style: TextStyle(
+                      color: Colors.grey[700]
+                    ),
+                  ),
+                  const SizedBox(width: 5),
+                  const Text("Register now",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold
+                    ),
+                  )
+                ],
+              )
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
